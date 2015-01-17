d65e68f9d1bcd3a1cc2318af448ddf9653f48d9a
tarecord123 <tanner.record@gmail.com>
2015-01-17 17:33:00 -0500

    added files to allow user authentication

---
+++ inc/templates/inc/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Swift Ticket</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+		
+	</head>
+	<body>
+
+	<?php include_once 'nav.php'; ?>
