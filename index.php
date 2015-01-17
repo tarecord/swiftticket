d34b246ff6968aa8a9a04bd91b3304f6fe39657c
tarecord123 <tanner.record@gmail.com>
2015-01-17 02:40:54 -0500

    added some basic markup to index.php

--- index.php
+++ index.php
@@ -2,13 +2,33 @@
 
 //Swift Tickets is a bare bones ticketing system based on php.
 
+include_once 'settings.php';
+
 //Include the class files as they are needed.
   function __autoload($class_name)
 	  {
 	      include_once 'inc/classes/' . $class_name . '.php';
 	  }
 	  
+	  //Initialize the Database Object.
 	  $db = new Db();
 	  
-	  $query = $db -> select("SELECT * FROM test");
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Swift Ticket</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+		
+	</head>
+	<body>
+		
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	</body>
+</html>
